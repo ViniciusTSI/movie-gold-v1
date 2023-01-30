700aae4155e5f095496f1083721f26ce49040712
Carlos Vinicius <carlosviniciustsi@outlook.com>
2023-01-29 21:34:37 -0300

    Create Home and Hero Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import api from './api/axiosConfig';
 import { useState, useEffect } from 'react';
+import Layout from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
 
@@ -10,8 +13,6 @@ function App() {
     
     try {
       const response = await api.get("/api/v1/movies");
-      
-      console.log(response.data);
       
       setMovies(response.data);
 
@@ -23,11 +24,18 @@ function App() {
 
   useEffect(() => {
     getMovies();
-}, []);
+},[])
 
 
   return (
     <div className="App">
+
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route path="/" element={<Home movies={movies}/>}></Route>
+
+        </Route>
+      </Routes>
       
     </div>
   );
